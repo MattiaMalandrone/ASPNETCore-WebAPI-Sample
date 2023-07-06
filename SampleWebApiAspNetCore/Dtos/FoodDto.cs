c1f2f9c3098921ef069439376e747fbf082afa01
Mattia Malandrone <mattia.malandrone@live.com>
2023-07-06 17:20:28 +0200

    data cahnges

--- SampleWebApiAspNetCore/Dtos/FoodDto.cs
+++ SampleWebApiAspNetCore/Dtos/FoodDto.cs
@@ -6,7 +6,7 @@
         public bool? IsPoisonous { get; set; }
         public string? Name { get; set; }
         public string? Type { get; set; }
-        public int Calories { get; set; }
+        public int? Calories { get; set; }
         public decimal Price { get; set; }
         public DateTime Created { get; set; }
     }
--- SampleWebApiAspNetCore/Services/SeedDataService.cs
+++ SampleWebApiAspNetCore/Services/SeedDataService.cs
@@ -7,20 +7,20 @@ namespace SampleWebApiAspNetCore.Services
     {
         public void Initialize(FoodDbContext context)
         {
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1000, Type = "Starter", Name = "Lasagne", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 1100, Type = "Main", Name = "Hamburger", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { Calories = 1200, Type = "Dessert", Name = "Spaghetti", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 1300, Type = "Starter", Name = "Pizza", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test1", Name = "Test1", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test2", Name = "Test2", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { Calories = 1300, Type = "Test3", Name = "Test3", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test4", Name = "Test4", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test5", Name = "Test5", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test6", Name = "Test6", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 1300, Type = "Test7", Name = "Test7", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test8", Name = "Test8", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test9", Name = "Test9", Created = DateTime.Now });
-            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 1300, Type = "Test10", Name = "Test10", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Type = "Starter", Name = "Lasagne", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 99, Type = "Main", Name = "Hamburger", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { Calories = 72, Type = "Dessert", Name = "Spaghetti", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 12, Type = "Starter", Name = "Pizza", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 25, Type = "Test1", Name = "Test1", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 100, Type = "Test2", Name = "Test2", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { Calories = 0, Type = "Test3", Name = "Test3", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 0, Type = "Test4", Name = "Test4", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 0, Type = "Test5", Name = "Test5", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 0, Type = "Test6", Name = "Test6", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = false, Calories = 0, Type = "Test7", Name = "Test7", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 0, Type = "Test8", Name = "Test8", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 2, Type = "Test9", Name = "Test9", Created = DateTime.Now });
+            context.FoodItems.Add(new FoodEntity() { IsPoisonous = true, Calories = 95, Type = "Test10", Name = "Test10", Created = DateTime.Now });
 
             context.SaveChanges();
         }
